5dcdb4503f8198b828e60d99fb2f25c9bf30c9ba
Arjun M <90825849+Arjun8420@users.noreply.github.com>
2021-11-16 17:51:58 +0530

    Add files via upload

---
+++ Array manipulation.py
@@ -0,0 +1,20 @@
+#Array manipulation
+import numpy as np
+
+a = np.array([[1,2],
+            [3,4]])
+
+# To print the minimum value
+print(a.min())
+
+# To print the maximum value
+print(a.max())
+
+# To print the Mean value
+print(a.mean())
+
+# To print the standard devation value
+print(a.std())
+
+# To print the variance value
+print(a.var())
